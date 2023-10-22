92e568acb10b44fc91027c5e67d27b89185bc110
FieldWorker <curcher@tuta.io>
2023-10-22 14:44:57 +0200

    retrieve the groups user is part of using telesint bot

--- interface.py
+++ interface.py
@@ -5,6 +5,7 @@ from socket import socket, SOCK_STREAM, AF_INET
 import requests
 import streamlit as st
 from telethon import TelegramClient
+import re
 
 from draw_graph.plot import draw_graph
 from main import DataManager
@@ -54,6 +55,84 @@ async def verify_otp(client, phone, secret_code, phone_hash):
         phone_code_hash=phone_hash,
     )
 
+async def send_message(client, message, user="me"):
+    await client.send_message(entity=user, message=message)
+
+
+async def get_messages(client, user="me"):
+    output=""
+    async for message in client.iter_messages(entity=user):
+        output += f"""{message.id}\n{message.text}\n"""
+        if message.buttons:
+            output += f"""{[button[0].text for button in message.buttons]}"""
+    return output
+
+    
+async def get_groups_of_which_user_is_part_of(client, user, dry_run=True):
+    """
+    interacts with telesint bot to query it for the groups, "user" is part of
+    client - telethon client
+    dry_run - only reads the message sent by telesint bot(good for testing)
+
+    """
+    failed_result = []
+    if not dry_run:
+        await client.send_message(entity="telesint_bot", message=user)
+    # example of an answer from the bot, specifically the message ends with
+    # search button, because the user is in db, otherwise here the result will
+    # be that the user is not found and the function returns with empty list
+    #  👨‍💼️️ Тип: пользователь
+
+    # 🆔 ID пользователя: 457528096
+
+    # 🔗 Ссылка: https://t.me/total_ordering
+
+    # 👤 Имя: Yegor 𓃰
+
+    # 🗃 Наличие в базе: ✅
+
+    # 🔍 Осталось запросов: 3
+    # ['🔍 Искать'] 
+
+
+    # if the user is in the database we need to click the button "Искать" (search)
+    if not dry_run:
+        async for message in client.iter_messages(entity="telesint_bot", limit=1):
+            if message.buttons:
+                await message.click(0)
+            else:
+                print("object not in the bot's db")
+                print(failed_result)
+
+    # now we want to read the output and extract the channels
+    # pattern used to extract the block of text containing groups names
+    answer_to_parse = ""
+    async for message in client.iter_messages(entity="telesint_bot", limit=1):
+        answer_to_parse = message.text
+    pattern_groups_text_block = r"(.|\n)*Открытые группы \[.*?\]:\n((.*\n?)*)" 
+    match_obj = re.match(pattern_groups_text_block, answer_to_parse)
+    text_block = ""
+    if match_obj:
+        text_block = match_obj.group(2)
+    else:
+        print("pattern not found in the message")
+        print(failed_result)
+
+    # example output after extraction of group 1 from regex
+    # "@ru_python Python
+    # @devops_ru DevOps — русскоговорящее сообщество
+    # @procxx pro.cxx
+    # ...
+    # @groupname <free text>"
+
+    pattern_extract_group_names_text = r"@(.*?)\s.*"
+    if re.match(pattern_extract_group_names_text, text_block):
+        group_names_text = re.sub(pattern_extract_group_names_text, r"\g<1>", match_obj.group(2))
+
+    return group_names_text.split("\n")
+
+# result = await get_groups_of_which_user_is_part_of(client_tg, "total_ordering", True)
+# print(result)
 
 phone_number_input = st.text_input(label='Phone numer', help='Input your phone number')
 api_id_input = st.text_input(label='Api id', help='Input your api id')
@@ -99,17 +178,25 @@ if hasattr(st.session_state, 'auth'):
                                       st.session_state.secret_code,
                                       st.session_state.phone_hash))
         st.session_state.auth = True
-group_id = None
+user_id = None
 button_clicked_load = None
+model_for_user_groups_exist = None
 if hasattr(st.session_state, 'auth'):
     if st.session_state.auth:
-        group_id = st.text_input(label='Input target group', help='id or group name')
-        button_clicked_load = st.button(label='Parse group')
-if group_id and button_clicked_load:
-    run_until_complete(
-        DataManager.load_data(client=st.session_state.client, channel=group_id))
-    st.write('**The data was loaded! Choose your params and click "show graph"**')
+        user_id = st.text_input(label='Input target user', help='id or group name')
+        button_clicked_load = st.button(label='Get the groups user is part of from telesint bot')
+if user_id and button_clicked_load:
+    # TODO here we should store the info we retrieved from telesint bot
+    # to a neo4j db
+    #  
+    # run_until_complete(
+        # DataManager.load_data(client=st.session_state.client, channel=group_id))
+    # st.write('**The data was loaded! Choose your params and click "show graph"**')
+    groups = run_until_complete(get_groups_of_which_user_is_part_of(st.session_state.client, user_id, dry_run=True))
+    st.write(groups)
+    st.write("implement the model for the user who're related by groups which theý're part of")
 # FETCH DATA WINDOW
+
 st.divider()
 st.write('**Fetch graph from storage**')
 col1, col2 = st.columns(2)
@@ -147,8 +234,9 @@ def show_on_server(G):
     else:
         st.write(response.text)
 
-
-if button_clicked_fetch:
+if button_clicked_fetch and not model_for_user_groups_exist:
+    st.write("we are missing proper model to represent this data")
+elif button_clicked_fetch and model_for_user_groups_exist:
     if arg:
         group_data = run_until_complete(DataManager.get_data(query_filter, int(arg)))
         fig, G = draw_graph(group_data, int(arg))
