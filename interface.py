4084e0ee9a806569a7c8f0ff5997db93b65abbdf
FieldWorker <curcher@tuta.io>
2023-12-19 21:26:34 +0000

    RDO-1 Add authentication for telesint bot

--- interface.py
+++ interface.py
@@ -10,6 +10,37 @@ from main import DataManager
 from streamlit_utils.text import query_hint
 from draw_graph.dynamic_plot import get_graph
 
+import hmac
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 
 def run_until_complete(coro):
     return st.session_state.event_loop.run_until_complete(coro)
